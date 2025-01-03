51961f6248db32874dd77d04b5fe2ea97cde4748
Daniel Krancz <daniel.krancz@clouddna.at>
2025-01-03 15:17:00 +0000

    Initial commit

---
+++ webapp/controller/Main.controller.ts
@@ -0,0 +1,34 @@
+import Controller from "sap/ui/core/mvc/Controller";
+import MessageBox from "sap/m/MessageBox";
+import MessageToast from "sap/m/MessageToast";
+import Input from "sap/m/Input";
+
+/**
+ * @namespace at.clouddna.demo.controller
+ */
+export default class Main extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    public onInit(): void {
+        let oView = this.getView(),
+            oInputFirstname = oView?.byId("inputFirstname") as Input;
+        oInputFirstname.setValue("Daniel");
+    }
+
+    private onSavePressed(){
+        MessageToast.show("Successfully saved.");
+      }
+
+      private onCancelPressed(){
+        MessageBox.warning("Are you sure, you want to cancel?", {
+          actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+          emphasizedAction: MessageBox.Action.YES, 
+          onClose: (sSelectedAction: String) => {
+            if(MessageBox.Action.YES === sSelectedAction){
+              MessageBox.success("Successfully cancelled.");
+            }
+            }
+        });
+      }
+      
+}
